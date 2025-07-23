a83f8464e5e99007918f2cc1c8ad458f92a89a53
kalyantalluri65 <147074859+kalyantalluri65@users.noreply.github.com>
2025-07-23 10:54:50 +0530

    Update Settings.tsx

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -44,7 +44,7 @@ function Settings() {
     setIsLoading(true)
 
     try {
-      const response = await fetch('http://localhost:3000/api/user/credentials', {
+      const response = await fetch('https://vehicle-parking-management.onrender.com/api/user/credentials', {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
